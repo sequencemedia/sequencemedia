488253b61fd74484d9f9df5549a59ce1f8d86c10
Jonathan Perry <jonathanperry@sequencemedia.net>
2025-04-05 12:38:25 +0100

    Husky

---
+++ .husky/common.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+get_package_changes () {
+  local CURRENT_BRANCH
+
+  CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+  git diff HEAD~1..HEAD~0 -- "origin/$CURRENT_BRANCH" package.json
+}
+
+has_package_changes () {
+  [ ! -z "$(get_package_changes)" ]
+}
+
+has_working_tree_changes () {
+  [ ! -z "$(git status pub -s)" ] || [ ! -z "$(git status src/sass -s)" ] # [ -n "$(git status pub -s)" ]
+}
